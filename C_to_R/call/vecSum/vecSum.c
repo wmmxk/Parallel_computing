3e065dcc688174dff4a1a91bb04d523e10263c51
wmmxk <wmmkx@hotmail.com>
2017-10-30 22:49:57 -0700

    call sum a vector

---
+++ C_to_R/call/vecSum/vecSum.c
@@ -0,0 +1,15 @@
+SEXP vecSum (SEXP Rvec) {
+   int i, n;
+   double *vec, value = 0;
+   vec = REAL(Rvec);
+   n = length(Rvec);
+
+   for (i=0; i < n; i++) 
+		   value += vec[i];
+
+   printf("The value is : %4.6f \n",value);
+   return R_NilValue;
+
+
+
+}
